a7ad39abc636d99560cf3b8dd0390702e702b928
Austin Morgan <127904226+kjrmorganaustin@users.noreply.github.com>
2026-09-05 08:35:14 -0400

    Removed comments

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,16 +1,13 @@
 javascript:(function(){
-/* invoke math random to make a random number from 1-100, if the number is 1 or less, then */
 var r = Math.random()*100;
 if(r<=1) {
-/* alert with a false error and redirect to SAO Alicization Lycoris subreddit */
 alert("ERR. Log Out Disabled. Project SEA TURTLE activated")
 window.location.replace("https://www.reddit.com/r/AlicizationLycoris/");
 } else 
-/* any other number */
 {
-/* Alert with “Welcome to Sword Art Online!”*/
-alert("Welcome to Sword Art Online!") 
-/* redirect to SAO subreddit */
+
+alert("Welcome to Sword Art Online!")
+
 window.location.replace("https://www.reddit.com/r/swordartonline/")
 }
 })();
